83434609d2bf0d75ceceef26c4f40e5c89b3dc52
帆 <1157712386@qq.com>
2024-01-30 18:06:01 +0800

    未修改

---
+++ WinNodeEditorDemo/Form2.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace WinNodeEditorDemo
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.stNodeEditor1 = new ST.Library.UI.NodeEditor.STNodeEditor();
+            this.SuspendLayout();
+            // 
+            // stNodeEditor1
+            // 
+            this.stNodeEditor1.AllowDrop = true;
+            this.stNodeEditor1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.stNodeEditor1.Curvature = 0.3F;
+            this.stNodeEditor1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.stNodeEditor1.Location = new System.Drawing.Point(0, 0);
+            this.stNodeEditor1.LocationBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.stNodeEditor1.MarkBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.stNodeEditor1.MarkForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.stNodeEditor1.MinimumSize = new System.Drawing.Size(100, 100);
+            this.stNodeEditor1.Name = "stNodeEditor1";
+            this.stNodeEditor1.Size = new System.Drawing.Size(800, 450);
+            this.stNodeEditor1.TabIndex = 0;
+            this.stNodeEditor1.Text = "stNodeEditor1";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.stNodeEditor1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ST.Library.UI.NodeEditor.STNodeEditor stNodeEditor1;
+    }
+}
